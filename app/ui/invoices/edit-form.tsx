18140b74b328dca82c9f5741500c28853daab26f
zwsutherland <zwsutherland@gmail.com>
2025-11-16 20:04:35 -0500

    please work

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -18,10 +18,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = async (formData: FormData) => {
+    await updateInvoice(invoice.id, formData);
+  };
 
-  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-
-  return ( <form action={updateInvoiceWithId}>
+  return (
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
