f153758d051a8dec8f3106bedae701694ea15f86
zwsutherland <zwsutherland@gmail.com>
2025-11-16 19:55:29 -0500

    13

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,7 +11,12 @@ import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form action={createInvoice}>
+    <form
+      action={async (formData: FormData) => {
+        'use server';
+        await createInvoice(formData);
+      }}
+    >
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
