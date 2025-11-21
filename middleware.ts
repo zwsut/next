ef7964f1e094b34ad3033c5705181f78fd3e51c8
zwsutherland <zwsutherland@gmail.com>
2025-11-20 19:02:13 -0500

    fix

---
+++ middleware.ts
@@ -0,0 +1,2 @@
+export { default } from './proxy';
+export { config } from './proxy';
--- proxy.ts
+++ proxy.ts
@@ -5,5 +5,5 @@ export default NextAuth(authConfig).auth;
  
 export const config = {
   // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
-matcher: ['/dashboard/:path*'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
